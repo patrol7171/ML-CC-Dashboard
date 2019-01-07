02d4012fedcb37807aeb95e1d3a284a44a95edf2
patrol7171 <patrol7171@gmail.com>
2019-01-07 17:37:41 -0500

    Update app.py

--- mlcc_dashboard/app.py
+++ mlcc_dashboard/app.py
@@ -83,13 +83,10 @@ db.session.commit()
 #################################################
 # Aeris Weather API Config
 #################################################
-# client_id = os.environ.get('client_id')
-# client_secret = os.environ.get('client_secret')
+client_id = os.environ.get('client_id')
+client_secret = os.environ.get('client_secret')
 app_id="com.aerisweather.pythonsdkdemo"
 
-########  FOR TESTING & LOCAL DEPLOY ONLY ##########
-client_id = "auufJLsp9vuvh5GzPT30w"
-client_secret = "mEPrGwuNvGYK5OwYrZLXb1HwdOUFAZEd2TsvihJ2" 
 	
 
 #################################################
