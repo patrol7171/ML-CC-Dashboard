4fa4a076e7c3a22c46950bf569d83700dccb580c
patrol7171 <patrol7171@gmail.com>
2019-01-08 14:27:09 -0500

    app file and html updates
    corrected app ID and updated the references page

--- mlcc_dashboard/app.py
+++ mlcc_dashboard/app.py
@@ -85,7 +85,7 @@ db.session.commit()
 #################################################
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
-app_id="com.aerisweather.pythonsdkdemo"
+app_id="herokuapp.com"
 
 	
 
